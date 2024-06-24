c47e5824f6f049f9953c759badbd5dcc86f4e80e
supartayasa <igstagungmadesupartayasa@gmail.com>
2024-06-24 14:20:49 +0800

    Add files via upload

---
+++ sekolah_1.sql
@@ -0,0 +1,170 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: sekolah
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `absensi`
+--
+
+DROP TABLE IF EXISTS `absensi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `absensi` (
+  `id_absensi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_siswa` int(11) NOT NULL,
+  `id_pelajaran` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` enum('H','I','S','A') NOT NULL,
+  PRIMARY KEY (`id_absensi`),
+  KEY `id_siswa` (`id_siswa`),
+  KEY `id_pelajaran` (`id_pelajaran`),
+  CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`),
+  CONSTRAINT `absensi_ibfk_2` FOREIGN KEY (`id_pelajaran`) REFERENCES `mata_pelajaran` (`id_pelajaran`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `absensi`
+--
+
+LOCK TABLES `absensi` WRITE;
+/*!40000 ALTER TABLE `absensi` DISABLE KEYS */;
+INSERT INTO `absensi` VALUES (11,112,2,'2024-06-25','I'),(12,112,1,'2024-06-24','H');
+/*!40000 ALTER TABLE `absensi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER absen_baru AFTER INSERT ON absensi
+
+FOR EACH ROW
+
+BEGIN
+
+  INSERT INTO rekap_absensi (id_siswa, id_pelajaran, hadir, ijin, sakit, alpha)
+
+  VALUES (NEW.id_siswa, NEW.id_pelajaran, 1, 0, 0, 0);
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `mata_pelajaran`
+--
+
+DROP TABLE IF EXISTS `mata_pelajaran`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mata_pelajaran` (
+  `id_pelajaran` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_pelajaran` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_pelajaran`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mata_pelajaran`
+--
+
+LOCK TABLES `mata_pelajaran` WRITE;
+/*!40000 ALTER TABLE `mata_pelajaran` DISABLE KEYS */;
+INSERT INTO `mata_pelajaran` VALUES (1,'biologi'),(2,'desain');
+/*!40000 ALTER TABLE `mata_pelajaran` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `rekap_absensi_siswa`
+--
+
+DROP TABLE IF EXISTS `rekap_absensi_siswa`;
+/*!50001 DROP VIEW IF EXISTS `rekap_absensi_siswa`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `rekap_absensi_siswa` AS SELECT
+ 1 AS `nis`,
+  1 AS `nama_lengkap`,
+  1 AS `nama_pelajaran`,
+  1 AS `hadir`,
+  1 AS `ijin`,
+  1 AS `sakit`,
+  1 AS `alpha` */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `siswa`
+--
+
+DROP TABLE IF EXISTS `siswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `siswa` (
+  `id_siswa` int(11) NOT NULL AUTO_INCREMENT,
+  `nis` varchar(10) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_siswa`),
+  UNIQUE KEY `nis` (`nis`)
+) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `siswa`
+--
+
+LOCK TABLES `siswa` WRITE;
+/*!40000 ALTER TABLE `siswa` DISABLE KEYS */;
+INSERT INTO `siswa` VALUES (112,'010','arik','12'),(113,'020','sup','11');
+/*!40000 ALTER TABLE `siswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `rekap_absensi_siswa`
+--
+
+/*!50001 DROP VIEW IF EXISTS `rekap_absensi_siswa`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `rekap_absensi_siswa` AS select `siswa`.`nis` AS `nis`,`siswa`.`nama_lengkap` AS `nama_lengkap`,`mata_pelajaran`.`nama_pelajaran` AS `nama_pelajaran`,sum(case when `absensi`.`status` = 'H' then 1 else 0 end) AS `hadir`,sum(case when `absensi`.`status` = 'I' then 1 else 0 end) AS `ijin`,sum(case when `absensi`.`status` = 'S' then 1 else 0 end) AS `sakit`,sum(case when `absensi`.`status` = 'A' then 1 else 0 end) AS `alpha` from ((`absensi` join `siswa` on(`absensi`.`id_siswa` = `siswa`.`id_siswa`)) join `mata_pelajaran` on(`absensi`.`id_pelajaran` = `mata_pelajaran`.`id_pelajaran`)) group by `siswa`.`nis`,`siswa`.`nama_lengkap`,`mata_pelajaran`.`nama_pelajaran` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-23 21:49:53
